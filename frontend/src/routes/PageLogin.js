f38f43b709f3402a6546de7b4c83e460c10b6f2b
Yohann Meyer <yohann.meyer@gmail.com>
2019-01-19 12:07:43 +0100

    frontend init

--- frontend/src/routes/PageLogin.js
+++ frontend/src/routes/PageLogin.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import FormLogin from '../components/FormLogin';
 
+const { BACKEND } = require('../config.js'); 
+
 const styles = theme => ({
   root: {
     padding: '120px 16px 16px',
@@ -17,7 +19,31 @@ const styles = theme => ({
 const PageLogin = ({ classes, history }) => {
   const handleSubmit = (values) => {
     console.log('submitting formValues', values);
-    history.push('/');
+    history.push('/login');
+    console.log(BACKEND.url);
+    
+    fetch(BACKEND.login, {
+      method: "post",
+      mode:"cors",
+    //  credentials: "include", // include, *same-origin, omit
+      headers: {
+        //'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    
+      body: JSON.stringify({ 
+        username: values.email,
+        password: values.password
+      
+      })
+    })
+    .then( (response) => { 
+      console.log(response);
+      document.cookie = `jwt_token=${response}; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/`;
+      console.log(document.cookie);
+       //do something awesome that makes the world a better place
+    });
+
   };
 
   return (
